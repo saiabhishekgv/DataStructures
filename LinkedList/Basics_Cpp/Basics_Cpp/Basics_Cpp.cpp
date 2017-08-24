ea73f72e52cca022dca9833da7e398f03d6bd9f6
Venkata Gogu <saiabhishekgv@gmail.com>
2017-08-23 20:32:55 -0400

    Code : Linked List basics in C++ Updated

---
+++ LinkedList/Basics_Cpp/Basics_Cpp/Basics_Cpp.cpp
@@ -0,0 +1,22 @@
+// Basics_Cpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "stdlib.h"
+
+// Implementing Stack in C++
+/* Stack implementation can be done using Dynamic arrays or Linked List.
+	properties - push and pop
+*/
+
+typedef struct Node {
+	struct Node *next;
+	int data;
+};
+
+int main()
+{
+    return 0;
+}
+
