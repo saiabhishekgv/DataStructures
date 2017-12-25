09447f76cf2ed2ebebce82ea3d65c23ba3871c34
Venkata Gogu <saiabhishekgv@gmail.com>
2017-12-24 18:03:18 -0600

    Code : LeetCode 3rd problem Longest Substring Without Repeating Characters

--- Algorithms/LeetCode/3-LongestSubstringWithoutRepeatingCharacters.py
+++ Algorithms/LeetCode/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -12,4 +12,50 @@ Given "bbbbb", the answer is "b", with the length of 1.
 
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+Solution :
+    1. Using HashMap - O(n^2)
+    2. Using HashMap using sliding Window - O(n)
 '''
+
+
+class Solution(object):
+    def lengthOfLongestSubstring_1(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        get_count = {}
+        counter = 0
+        max_length = 0
+
+        for index, i in enumerate(s):
+            get_count[i] = 1
+            current_length = 1
+            for j in xrange(index + 1, len(s)):
+                if s[j] in get_count:
+                    break
+                else:
+                    get_count[s[j]] = 1
+                    current_length += 1
+            max_length = max_length if max_length > current_length else current_length
+            get_count.clear()
+
+        return max_length
+
+    def lengthOfLongestSubstring_2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        get_count = {}
+        ans = 0
+        i = 0
+
+        for j in xrange(len(s)):
+            if s[j] in get_count:
+                i = max(get_count[s[j]], i)
+            ans = max(ans, j - i + 1)
+            get_count[s[j]] = j + 1
+
+        return ans
+    
