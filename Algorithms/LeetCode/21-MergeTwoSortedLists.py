5de54acb8ebe25e59df1fed08faa73212e89947c
Venkata Gogu <saiabhishekgv@gmail.com>
2017-12-29 19:41:58 -0600

    Code : LeetCode Question 21. Merge Two Sorted Lists

--- Algorithms/LeetCode/21-MergeTwoSortedLists.py
+++ Algorithms/LeetCode/21-MergeTwoSortedLists.py
@@ -24,6 +24,11 @@ Solution :
         - Run time : 56 ms
 '''
 
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
