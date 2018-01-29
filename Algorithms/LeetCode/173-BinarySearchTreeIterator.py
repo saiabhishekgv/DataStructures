7070c1c3d3705be50a918de6232a104b19a27314
Venkata Gogu <saiabhishekgv@gmail.com>
2018-01-29 01:35:51 -0600

    Code : LeetCode Question 173. Binary Search Tree Iterator

--- Algorithms/LeetCode/173-BinarySearchTreeIterator.py
+++ Algorithms/LeetCode/173-BinarySearchTreeIterator.py
@@ -19,11 +19,11 @@ Solution :
 
 
 # Definition for a  binary tree node
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class BSTIterator(object):
     def __init__(self, root):
