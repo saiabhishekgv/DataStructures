86eb767195c9f2509278c3e9647b40cae4426332
Venkata Gogu <saiabhishekgv@gmail.com>
2018-02-15 18:21:16 -0600

    Code : Leetcode Question 677. Map Sum Pairs

---
+++ Algorithms/LeetCode/677-MapSumPairs.py
@@ -0,0 +1,71 @@
+'''
+Question 677. Map Sum Pairs
+
+Implement a MapSum class with insert, and sum methods.
+
+For the method insert, you'll be given a pair of (string, integer). The string represents the key and the integer represents the value. If the key already existed, then the original key-value pair will be overridden to the new one.
+
+For the method sum, you'll be given a string representing the prefix, and you need to return the sum of all the pairs' value whose key starts with the prefix.
+
+Example 1:
+Input: insert("apple", 3), Output: Null
+Input: sum("ap"), Output: 3
+Input: insert("app", 2), Output: Null
+Input: sum("ap"), Output: 5
+'''
+'''
+References:
+    1.
+    2.
+'''
+'''
+Solution : 
+    1. Brute Force : 
+        - Time Complexity : O(N * P) 
+    2. Prefix Hashmap :
+        - Time COmplexity : O(k^2)
+    3. Trie : 
+        - Time Complexity : O(K)
+        - Space Complexity : O(K) ==> k = length of Key
+        
+'''
+
+class MapSum_1(object):
+    def __init__(self):
+        self.map = {}
+
+    def insert(self, key, val):
+        self.map[key] = val
+
+    def sum(self, prefix):
+        return sum(val for key, val in self.map.items()
+                   if key.startswith(prefix))
+
+
+class TrieNode(object):
+    __slots__ = 'children', 'score'
+    def __init__(self):
+        self.children = {}
+        self.score = 0
+
+class MapSum(object):
+    def __init__(self):
+        self.map = {}
+        self.root = TrieNode()
+
+    def insert(self, key, val):
+        delta = val - self.map.get(key, 0)
+        self.map[key] = val
+        cur = self.root
+        cur.score += delta
+        for char in key:
+            cur = cur.children.setdefault(char, TrieNode())
+            cur.score += delta
+
+    def sum(self, prefix):
+        cur = self.root
+        for char in prefix:
+            if char not in cur.children:
+                return 0
+            cur = cur.children[char]
+        return cur.score
