55ac273d0db70d3e067cbdd1cf0884e4bbe5594d
Venkata Gogu <saiabhishekgv@gmail.com>
2018-03-03 12:48:02 -0600

    Code : Question Question 791. Custom Sort String

---
+++ Algorithms/LeetCode/791-Custom Sort String.py
@@ -0,0 +1,61 @@
+'''
+Leetcode Question 791. Custom Sort String
+
+S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they match the order that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+
+Return any permutation of T (as a string) that satisfies this property.
+
+Example :
+Input:
+S = "cba"
+T = "abcd"
+Output: "cbad"
+Explanation:
+"a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a".
+Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
+
+
+Note:
+
+S has length at most 26, and no character is repeated in S.
+T has length at most 200.
+S and T consist of lowercase letters only.
+
+'''
+
+'''
+Referenes : 
+    1. https://leetcode.com/problems/custom-sort-string/solution/
+    2. 
+'''
+
+'''
+Solution : 
+    1. Count and append :
+        Time - O(m + n)
+        Run time - 48 ms
+    2.
+'''
+import collections
+class Solution(object):
+    def customSortString(self, S, T):
+        # count[char] will be the number of occurrences of
+        # 'char' in T.
+        count = collections.Counter(T)
+        ans = []
+
+        # Write all characters that occur in S, in the order of S.
+        for c in S:
+            ans.append(c * count[c])
+            # Set count[c] = 0 to denote that we do not need
+            # to write 'c' to our answer anymore.
+            count[c] = 0
+
+        # Write all remaining characters that don't occur in S.
+        # That information is specified by 'count'.
+        for c in count:
+            ans.append(c * count[c])
+
+        return "".join(ans)
